02f804573047e016c2a28192886f4523520ebee7
Giampietro Pregnolato <g.pregnolato@egeatech.com>
2021-06-06 19:51:52 +0200

    removed unused files

--- igFocus/IG Focus 01 2021 Cover Story_files/teleborsa.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-		url: "https://syndication.teleborsa.it/Italgas/Feeds/jsonValues", 
-		method: "get",		
-		success: function(result){
-        	$("#title-perc").text(result.percentChange);
-			$("#title-price").text(result.lastTrade);
-		}
-    });
