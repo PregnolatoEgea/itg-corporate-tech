0b09a871182d6929208b24e54d9b9f2b6b850116
Giampietro Pregnolato <g.pregnolato@egeatech.com>
2021-06-14 06:30:15 +0200

    Media filter update

--- itg_sustainability/asp/filters/taxonomy/asp-tax-radio.php
+++ itg_sustainability/asp/filters/taxonomy/asp-tax-radio.php
@@ -19,29 +19,29 @@
             
             
         <?php if ($taxonomy === 'post_tag') : ?>
-        
-            <div class="is-2 itgtag-filter">
+            <div class="column is-2 itgtag-filter is-pulled-right">
               <a id="itg_tagfilterbtn" href="#">Argomento</a>
             </div>
-            <div class="is-2 itgtag-filter">
+            <div class="column is-2 itgtag-filter is-pulled-right">
               <a id="itg_tagfilterbtn" href="#">Periodo</a>
             </div>
             <div class="is-clearfix"></div>
             <div class="container">
-             <div id="itg_tagfilterresults" class="column is-10 is-offset-2 pt-5" style="display: none;">
-              <div class="columns">
-               <?php foreach ($filter->get() as $kk => $term): ?>
-                <div class="itg_asp_label aligncenter <?php echo $taxonomy; ?>_filter_box itgtagfilter column is-4">
-                 
-                   <input type="radio" class="asp_radio"
+             <div id="itg_tagfilterresults" class="columns pt-5" style="display: none;">
+              <div class="column is-10 is-offset-2">
+               <div class="columns">
+                 <?php foreach ($filter->get() as $kk => $term): ?>
+                   <div class="itg_asp_label aligncenter <?php echo $taxonomy; ?>_filter_box itgtagfilter column is-3">
+                    <input id="itg_<?php echo $ch_class; ?>_sub-<?php echo $term->id; ?>" type="checkbox" class="asp_checkbox"
                           value="<?php echo $term->id; ?>"
                        <?php echo $term->default ? 'data-origvalue="1"' : ''; ?>
                           name='<?php echo $filter->isMixed() ? "termset_single" : "termset[" . $taxonomy . "][]"; ?>'
                        <?php echo $term->selected ? ' checked="checked"' : ''; ?>  aria-hidden="true">
-                   <?php echo $term->label; ?>
-                 <label for="<?php echo $term->id; ?>"></label>
-                </div>
-               <?php endforeach; ?>
+                   
+                       <label for="itg_<?php echo $ch_class; ?>_sub-<?php echo $term->id; ?>"><?php echo $term->label; ?></label>
+                     </div>
+                  <?php endforeach; ?>
+               </div>
               </div>
              </div>
             </div>
