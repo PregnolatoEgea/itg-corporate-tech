da5e0c4e4e2c37b2065f5467dbb8ff57d3c5057a
Giampietro Pregnolato <g.pregnolato@egeatech.com>
2021-06-14 12:29:06 +0200

    update

--- itg_sustainability/asp/filters/taxonomy/asp-tax-radio.php
+++ itg_sustainability/asp/filters/taxonomy/asp-tax-radio.php
@@ -23,7 +23,7 @@
               <a id="itg_tagfilterbtn" href="#">Argomento</a>
             </div>
             <div class="column is-2 itgtag-filter is-pulled-right">
-              <a id="itg_tagfilterbtn" href="#">Periodo</a>
+              <a id="itg_datefilterbtn" href="#">Periodo</a>
             </div>
             <div class="is-clearfix"></div>
             <div class="container">
@@ -42,10 +42,80 @@
                      </div>
                   <?php endforeach; ?>
                </div>
+               <div class="columns">
+                <div class="column is-3 is-offset-3 itg_applyfilters aligncenter">
+                 <a href="#">Applica filtri</a>
+                </div>
+                <div class="column is-3 is-offset-3 itg_resetfilters aligncenter">
+                 <a href="#">Azzera filtri X</a>
+                </div>
+               </div>
               </div>
              </div>
             </div>
             
+            
+            <!-- date range filter -->
+             <div id="itg_dateRangefilterresults" class="columns pt-5" style="display: none;">
+               <div class="columns">
+                <div class="column is-4">
+                
+                </div>
+                
+                <div class="column is-4">
+                 <?php foreach($filter->get() as $date): ?>
+                    <div class="asp_<?php echo esc_attr($date->name); ?>">
+                    <?php if ( $date->label != "" ): ?>
+                    <legend><?php echo esc_html($date->label); ?></legend>
+                    <?php endif; ?>
+                    <textarea class="asp_datepicker_format"
+                              aria-hidden="true"
+                              aria-label="<?php echo esc_attr($date->label); ?>"
+                              style="display:none !important;"><?php echo esc_html($date->format); ?></textarea>
+                    <input type="text"
+                           aria-label="<?php echo esc_attr($date->label); ?>"
+                           placeholder="<?php echo esc_attr($date->placeholder); ?>"
+                           class="asp_datepicker"
+                           name="<?php echo esc_attr($date->name); ?>_real"
+                           data-origvalue="<?php echo esc_attr($date->default); ?>"
+                           value="<?php echo esc_attr($date->value); ?>">
+                    <input type="" class="asp_datepicker_hidden" name="<?php echo esc_attr($date->name); ?>" value="">
+                    </div>
+                 <?php endforeach; ?>
+                </div>
+                
+                <div class="column is-4">
+                 <?php foreach($filter->get() as $date): ?>
+                    <div class="asp_<?php echo esc_attr($date->name); ?>">
+                    <?php if ( $date->label != "" ): ?>
+                    <legend><?php echo esc_html($date->label); ?></legend>
+                    <?php endif; ?>
+                    <textarea class="asp_datepicker_format"
+                              aria-hidden="true"
+                              aria-label="<?php echo esc_attr($date->label); ?>"
+                              style="display:none !important;"><?php echo esc_html($date->format); ?></textarea>
+                    <input type="text"
+                           aria-label="<?php echo esc_attr($date->label); ?>"
+                           placeholder="<?php echo esc_attr($date->placeholder); ?>"
+                           class="asp_datepicker"
+                           name="<?php echo esc_attr($date->name); ?>_real"
+                           data-origvalue="<?php echo esc_attr($date->default); ?>"
+                           value="<?php echo esc_attr($date->value); ?>">
+                    <input type="" class="asp_datepicker_hidden" name="<?php echo esc_attr($date->name); ?>" value="">
+                    </div>
+                <?php endforeach; ?>
+                </div>
+               </div>
+               
+               <div class="is-clearfix"></div>
+               <div class="columns">
+                <div class="column is-3 is-offset-3 itg_applyfilters aligncenter">
+                 <a href="#">Applica filtri</a>
+                </div>
+                <div class="column is-3 is-offset-3 itg_resetfilters aligncenter">
+                 <a href="#">Azzera filtri X</a>
+                </div>
+               </div>
             
         <?php endif; ?>
 
@@ -53,6 +123,16 @@
           jQuery('#itg_tagfilterbtn').click(function(e){
            jQuery(this).toggleClass('active');
            jQuery('#itg_tagfilterresults').toggle();
+           jQuery('#itg_dateRangefilterresults').hide();
+           jQuery('#itg_datefilterbtn').removeClass('active');
+           e.preventDefault();
+          });
+          
+          jQuery('#itg_datefilterbtn').click(function(e){
+           jQuery(this).toggleClass('active');
+           jQuery('#itg_dateRangefilterresults').toggle();
+           jQuery('#itg_tagfilterresults').hide();
+           jQuery('#itg_tagfilterbtn').removeClass('active');
            e.preventDefault();
           });
          </script>
