2caca1ef838136f065832e0c3985923193b64268
Luca <luca@ahd-creative.com>
2021-06-18 12:25:08 +0200

    Create share.php

---
+++ itg_corporate/share.php
@@ -0,0 +1,11 @@
+<?php
+$url=get_permalink();
+?>
+<a href="https://www.linkedin.com/cws/share?url=".<?php echo $url; ?>>SVG Linkedin</a>
+<a href="http://twitter.com/share" class="twitter-share-button"
+   data-url="<?php echo $url; ?>"
+   data-via="italgas"
+   data-text="<?php the_title(); ?>"
+   data-related="Italgas"
+   data-count="vertical">SVG Twtitter</a>
+<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
