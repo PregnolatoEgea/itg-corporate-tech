5e06948702ec9273576cef8901f0c96fff6606d2
Samuele Orfei <samuele.orfei@outlook.it>
2021-06-28 17:54:04 +0200

    Update footer.php

--- itg_corporate/footer.php
+++ itg_corporate/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -14,89 +15,48 @@ $footer_logo = get_field('footer_logo', 'option');
 $sub_footer_left_copy = get_field('sub_footer_left_copy', 'option');
 $sub_footer_right_copy = get_field('sub_footer_right_copy', 'option');
 $footer_menu = wp_get_nav_menu_items('footer-menu');
-$i=0;
+$i = 0;
 $wrap_count_first_level = 4;
 ?>
 
 <footer id="colophon" class="site-footer">
     <div class="section is-paddingless">
         <div class="itgFooter container itg-py-56">
-            <div class="columns">
-                <div class="itgFooter__left-container columns is-5-desktop is-4-tablet">
-                    <?php
-                    if($footer_logo){
-                        ?>
-                        <div class="itgFooter__logo column is-5-desktop">
-                            <img src="<?php echo $footer_logo['url']; ?>" alt="<?php echo $footer_logo['alt']; ?>">
-                        </div>
-                        <?php
-                    }
-                    ?>
-                    <div class="itgFooter__menu column is-7-desktop is-offset-1-desktop">
-                        <?php $navArgs = array('footer-menu' => new footer_menu_walker());
-                        wp_nav_menu($navArgs);
-                        ?>
-                    </div>
-                </div>
+            <div class="itgFooter__left-container is-5-desktop is-4-tablet">
                 <?php
-                if( have_rows('footer_social', 'option') ):
+                if ($footer_logo) {
                 ?>
-                <div class="itgFooter__social column is-7-desktop is-4-tablet is-offset-1-desktop">
-                    <?php
-
-                    while( have_rows('footer_social', 'option') ) : the_row();
-
-                    $footer_social_title = get_sub_field('title');
-                    $footer_social_copy = get_sub_field('copy');
-
-                    ?>
-
-                    <div class="itgFooter__social--title itg-mb-8"><?php echo $footer_social_title; ?></div>
-                    <div class="itgFooter__social--copy itg-mb-8"><?php echo $footer_social_copy; ?></div>
-                    <div class="itgFooter__social--icons">
-                        <?php
-                        if( have_rows('social_icons', 'option') ):
-                        ?>
-                        <div class="itgFooter__social--icon">
-                            <?php
-                            while( have_rows('social_icons', 'option') ) : the_row();
-                                $social_icon_link = get_sub_field('link');
-                                $social_icon_icon = get_sub_field('icon');
-                                ?>
-                                <a class="itg-mr-8" href="<?php echo $social_icon_link['url']; ?>"><img src="<?php echo $social_icon_icon['url']; ?>" alt="<?php echo $social_icon_icon['alt']; ?>"></a>
-                            <?php
-                            endwhile;
-                            endif;
-                            ?>
-                        </div>
-                        <?php
-
-                        endwhile;
-
-                        endif;
-                        ?>
+                    <div class="itgFooter__logo is-5-desktop">
+                        <img src="<?php echo $footer_logo['url']; ?>" alt="<?php echo $footer_logo['alt']; ?>">
                     </div>
+                <?php
+                }
+                ?>
+                <div class="itgFooter__menu is-7-desktop is-offset-1-desktop">
+                    <?php $navArgs = array('footer-menu' => new footer_menu_walker());
+                    wp_nav_menu($navArgs);
+                    ?>
                 </div>
             </div>
         </div>
         <div class="itgSubFooter itg-px-56">
             <?php
-            if($sub_footer_left_copy){
-                ?>
+            if ($sub_footer_left_copy) {
+            ?>
                 <div class="itgSubFooter__leftSide"><?php echo $sub_footer_left_copy; ?></div>
-                <?php
+            <?php
             }
-            if( have_rows('sub_footer_menu', 'option') ):
-                ?>
+            if (have_rows('sub_footer_menu', 'option')) :
+            ?>
                 <div class="itgSubFooter__centerSide">
                     <?php
 
-                    while( have_rows('sub_footer_menu', 'option') ) : the_row();
+                    while (have_rows('sub_footer_menu', 'option')) : the_row();
 
                         $sub_footer_menu_label = get_sub_field('label');
                         $sub_footer_menu_link = get_sub_field('link');
 
-                        ?>
+                    ?>
 
                         <a href="<?php echo $sub_footer_menu_link['url']; ?>"><?php echo $sub_footer_menu_label; ?></a>
 
@@ -107,10 +67,10 @@ $wrap_count_first_level = 4;
                 </div>
             <?php
             endif;
-            if($sub_footer_right_copy){
-                ?>
+            if ($sub_footer_right_copy) {
+            ?>
                 <div class="itgSubFooter__rightSide"><?php echo $sub_footer_right_copy; ?></div>
-                <?php
+            <?php
             }
             ?>
         </div>
@@ -121,4 +81,5 @@ $wrap_count_first_level = 4;
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
