52c834b9307280822ae911b33c35da45c46aa85f
Guidolin Marco <m.guidolin@egeatech.com>
2021-07-01 16:09:07 +0200

    FIX file indentation

--- itg_corporate/js/components/animations/ItgFooter.js
+++ itg_corporate/js/components/animations/ItgFooter.js
@@ -1,33 +1,32 @@
-export const ItgFooter = function ()
-{
-		let footerAnchorContainer = document.getElementById('menu-footer-menu');
-  let elementClicked = footerAnchorContainer.querySelectorAll(".menu-item-has-children>a");
-		let itgSubmenus = document.querySelector("[class*=sub-menu]");
-		let itgSubmenusToggler = document.querySelector(".ItgFooter__toggleBtn");
+export const ItgFooter = function () {
+	// Tablet and up constraints
+	let footerAnchorContainer = document.getElementById('menu-footer-menu');
+	let elementClicked = footerAnchorContainer.querySelectorAll(".menu-item-has-children>a");
+	let itgSubmenus = document.querySelector("[class*=sub-menu]");
+	let itgSubmenusToggler = document.querySelector(".ItgFooter__toggleBtn");
 
+	elementClicked.forEach(function ($el) {
 
-		elementClicked.forEach(function ($el) {
-			
-      $el.addEventListener('click', function () {
-	      event.preventDefault();
-							let itgSubmenus = document.querySelectorAll("[class*=sub-menu]");
+		$el.addEventListener('click', function () {
+			event.preventDefault();
+			let itgSubmenus = document.querySelectorAll("[class*=sub-menu]");
 
-							for (var i = 0; i < itgSubmenus.length; i++) {
-							    itgSubmenus[i].classList.toggle("is-show");
-							}    
-							
-      });
-    });
-    
-  itgSubmenusToggler.addEventListener('click', function () {
-	  	event.preventDefault();
-	  	let itgSubmenus = document.querySelectorAll("[class*=sub-menu]");
-				for (var i = 0; i < itgSubmenus.length; i++) {
-							    itgSubmenus[i].classList.toggle("is-show");
-						} 
+			for (var i = 0; i < itgSubmenus.length; i++) {
+				itgSubmenus[i].classList.toggle("is-show");
+			}
+
 		});
+	});
 
-}
+	itgSubmenusToggler.addEventListener('click', function () {
+		event.preventDefault();
+		let itgSubmenus = document.querySelectorAll("[class*=sub-menu]");
+		for (var i = 0; i < itgSubmenus.length; i++) {
+			itgSubmenus[i].classList.toggle("is-show");
+		}
+	});
+
+};
 
 ItgFooter();
 
