2356a1ddbbb9ce975bc700be173211c1b413e395
Samuele Orfei <samuele.orfei@outlook.it>
2021-07-03 10:23:41 +0200

    Update ItgHeroImageSwiper.js

--- itg_corporate/js/components/blocks/ItgHeroImageSwiper.js
+++ itg_corporate/js/components/blocks/ItgHeroImageSwiper.js
@@ -2,9 +2,7 @@ import Swiper from 'swiper';
 
 export const ItgHeroImageSwiper = function ()
 {
-    console.log("Swiper Hero");
     let ItgHeroImageContainers = document.querySelector('.itgBlock-hero-image-slider');
-    console.log(ItgHeroImageContainers);
 
     let mySwiper = new Swiper(ItgHeroImageContainers, {
         direction: 'horizontal',
@@ -15,7 +13,6 @@ export const ItgHeroImageSwiper = function ()
             el: ".swiper-pagination",
             renderBullet: function (index, className)
             {
-                console.log("Render bullet");
                 return '<span class="' + className + '">' + "</span>";
             },
         },
