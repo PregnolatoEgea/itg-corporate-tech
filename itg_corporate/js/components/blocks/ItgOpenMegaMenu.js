4ca6ec701a021c93e644bf0e31caaed573f82ac3
Samuele Orfei <samuele.orfei@outlook.it>
2021-07-05 18:38:40 +0200

    Update ItgOpenMegaMenu.js

--- itg_corporate/js/components/blocks/ItgOpenMegaMenu.js
+++ itg_corporate/js/components/blocks/ItgOpenMegaMenu.js
@@ -1,38 +1,59 @@
+import { ids } from "webpack";
 
 export const ItgOpenMegaMenu = function (key) 
 {
-window.addEventListener("load", function() {
+  window.addEventListener("load", function ()
+  {
 
-const tabs = Array.from(document.querySelectorAll(".navbar-trigger"));
+    const tabs = Array.from(document.querySelectorAll(".navbar-trigger"));
 
-const tabContents = document.querySelectorAll(".navbar-dropdown");
+    const tabContents = document.querySelectorAll(".navbar-dropdown");
 
-tabs.forEach(function(tab){
-  tab.addEventListener("click", switchClass);
-  event.preventDefault();
-});
+    const narrow = document.querySelectorAll(".is-narrow");
 
-let priorActive = null; 
+    tabs.forEach(function (tab)
+    {
+      tab.addEventListener("click", switchClass);
+      event.preventDefault();
+    });
 
-function switchClass() {
-  // Loop over the tabs
-  tabs.forEach(function(tab, index){
-    tab.classList.remove("is-active");            
-    tabContents[index].classList.add("is-hide");
-  });
-  
-  if(priorActive === this){
-    this.classList.remove("is-active");  
-    tabContents[tabs.indexOf(this)].classList.add("is-hide");
-    priorActive = null;
-  } else {
-    this.classList.add("is-active");  
-    tabContents[tabs.indexOf(this)].classList.remove("is-hide");  
-    priorActive = this;
-  }
-}
+    let priorActive = null;
 
-})
+    function switchClass()
+    {
+      narrow.forEach(function (tab, index)
+      {
+        if (tab.classList.contains('is-active'))
+        {
+          tab.classList.remove('is-active');
+        }
+      });
+
+      if (narrow[0])
+      {
+        narrow[0].classList.add('is-active');
+      }
+      // Loop over the tabs
+      tabs.forEach(function (tab, index)
+      {
+        tab.classList.remove("is-active");
+        tabContents[index].classList.add("is-hide");
+      });
+
+      if (priorActive === this)
+      {
+        this.classList.remove("is-active");
+        tabContents[tabs.indexOf(this)].classList.add("is-hide");
+        priorActive = null;
+      } else
+      {
+        this.classList.add("is-active");
+        tabContents[tabs.indexOf(this)].classList.remove("is-hide");
+        priorActive = this;
+      }
+    }
+
+  })
 }
 
 
